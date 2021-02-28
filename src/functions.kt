7fb273df1e71562ded655113951bd2e364e28b14
Euniceatieno <eunniceatieno@gmail.com>
2021-02-28 12:09:00 +0300

    second assignmenet

---
+++ src/functions.kt
@@ -0,0 +1,31 @@
+
+fun main() {
+    myName()
+    println(50%23)
+    println(10+20+30+40)
+    mySelf()
+
+}
+fun myName(){
+    var myName="Eunice"
+    println("Hello $myName")
+}
+fun modulus(a:Int, b:Int):Int {
+    return(a%b)
+}
+
+fun sum(num1:Int, num2:Int, num3:Int, num4:Int): Int
+{
+    return(num1+num2+num3+num4)
+
+
+}
+fun mySelf(){
+    println("I have fallen in love with coding and I am open to learning any programming language.")
+}
+
+
+
+
+
+
